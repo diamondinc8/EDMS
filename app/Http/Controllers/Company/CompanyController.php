7b633ebdbaedbc3a9e22ebff26eaedb5350790d2
diamondinc__ <diamondinc7@bk.ru>
2025-08-20 23:14:37 +0300

    Добавлены комментарии в CompanyController.php

--- app/Http/Controllers/Company/CompanyController.php
+++ app/Http/Controllers/Company/CompanyController.php
@@ -11,16 +11,20 @@ use App\Services\CompanyService;
 class CompanyController extends BaseController
 {
 
+    // Открытие формы для заполнения информации о компании
     public function create()
     {
         return view('company.create');
     }
 
+    // Создание компании
     public function store(CompanyRequest $request)
     {
+        // Получение данных из формы
         $data = $request->validated();
+        // Создание компании в SQl
         $this->service->createCompany($data);
-
+        // Переадресация на главную страницу
         return redirect()->route('company.index');
     }
 }
